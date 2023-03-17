3576ae4d296e602a3f12f518372880a2ed952c2f
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-03-17 12:36:11 -0700

    Adds index.html

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,13 +6,12 @@ const { expect } = chai;
 
 describe("index.html", () => {
   it("contains an <a> tag", () => {
-    const aTag = document.querySelector("a");
-    expect(aTag).to.exist;
+   const aTag = document.querySelector("a"); 
+   expect(aTag).to.exist
   });
-
-  it("has the text content of 'Flatiron School'", () => {
-    const aTag = document.querySelector("a");
-    expect(aTag).to.have.text("Flatiron School");
+   it("has the text content of 'Flatiron School'", () => {
+   const aTag = document.querySelector("a");
+   expect(aTag).to.have.text("Flatiron School");
   });
 
   it("has the href attribute of 'https://flatironschool.com'", () => {
